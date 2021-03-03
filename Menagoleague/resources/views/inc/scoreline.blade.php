bf47796c0f66bb6fa5b1013da05508703478ab4b
Daniel <danielheinze96@gmail.com>
2021-03-03 12:52:42 +0100

    storage-link

--- Menagoleague/resources/views/inc/scoreline.blade.php
+++ Menagoleague/resources/views/inc/scoreline.blade.php
@@ -3,7 +3,7 @@
     @for($i = 1; $i<=20; $i++) <div class="game d-flex flex-column  align-items-center">
 
         <div class="team d-flex flex-row justify-content-between align-items-center">
-            <span class="shield"><img src="5.png"></span><span class="host">Arsenal</span> <span class="goals">1</span>
+            <span class="shield"><img src="{{asset('shields/5.jpg')}}"></span><span class="host">Arsenal</span> <span class="goals">1</span>
         </div>
 
         <div class="team d-flex flex-row justify-content-between align-items-center">
