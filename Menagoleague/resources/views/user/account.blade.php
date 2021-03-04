cfb26556f305cf6b92bf05ace93a8f9e591139cd
Witek Milewski <milewskiwitek8@gmail.com>
2021-03-04 10:09:56 +0100

    updating-welcome-page

--- Menagoleague/resources/views/user/account.blade.php
+++ Menagoleague/resources/views/user/account.blade.php
@@ -47,7 +47,6 @@
                             </tr>
                         </tbody>
                     </table>
-                    {{$personal_data->team->players}}
                     <form method="POST" action="{{route('account.update')}}">
                         @csrf
                         <input type="text" maxlength="4" name="discord" id="discord">
