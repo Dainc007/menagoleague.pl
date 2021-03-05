e5eb36f0b26755d2bbbfa9f29b22447cae337596
Daniel <danielheinze96@gmail.com>
2021-03-05 16:25:10 +0100

    register fix

--- Menagoleague/app/Http/Controllers/Auth/RegisterController.php
+++ Menagoleague/app/Http/Controllers/Auth/RegisterController.php
@@ -54,8 +54,6 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'discord' => ['integer', 'unique:users'],
-            'facebook' => ['string', 'max:255'],
         ]);
     }
 
--- Menagoleague/resources/views/auth/passwords/email.blade.php
+++ Menagoleague/resources/views/auth/passwords/email.blade.php
@@ -18,7 +18,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adres E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Resetuj hasło') }}
                                 </button>
                             </div>
                         </div>
