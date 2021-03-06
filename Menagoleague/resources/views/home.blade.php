aba868767d21b5513e89b4aa52370f102bf11cac
Daniel <danielheinze96@gmail.com>
2021-03-06 13:13:14 +0100

    readme file edited

--- Menagoleague/resources/views/home.blade.php
+++ Menagoleague/resources/views/home.blade.php
@@ -1,18 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+.user-info-wrapper{
+    padding: calc(1.5vw + 10px);
+    display: flex;
+}
+.user-info{
+    display: flex;
+    background-color:white;
+
+    padding:8px;
+     
+    }
+    img{
+            width:70px;
+            height:70px;
+        }
+    .team-and-name{
+        font-size: 18px;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: flex-start;
+    }
+
+
+.user-morale{
+    display: flex;
+    justify-content: center;
+    padding: 0 5px;
+    align-items: center;
+    font-size: 2.6rem;
+    background-color: gray;
+    color:white;
+}
+</style>
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
+
+<!--             <div class="bg-dark d-flex" style="width:400px; height:120px;">
+                <div class="bg-success my-auto" style="width:100px; height:100px;">
+                    <span><img class="img-fluid" src="{{ asset('storage/shields/5.png') }}"></span>
+                </div>
+                <div class="d-flex flex-column my-auto">
+                    <div class="bg-primary d-flex" style="width:100px; height:50px;"><span class="align-middle">OK</span> </div>
+                    <div class="bg-warning" style="width:100px; height:50px;"> <span>Manager</span> </div>
+                </div>
+                <div class="bg-success my-auto" style="width:100px; height:100px;"> </div>
+            </div> -->
+
+            <div class="user-info-wrapper">
+                <div class="user-info">
+                    <div class="user-info logo">
+                        <img src="{{ asset('storage/shields/5.png') }}" alt="herb">
+                    </div>
+                    <div class="user-info__team-and-name">
+                        <span class="user-name">
+                            SnaggyDainc </span>
+                        <span class="team-name">
+                            Arsenal </span>
+                    </div>
+                </div>
+                <div class="user-morale" title="ogólno rozumiany poziom zadowolenia panujący w klubie">
+                    100 </div>
+
+                <div class="user-rivals">
+                    <span class="rival"></span>
+                </div>
+            </div>
+
             <div class="card">
                 <div class="card-header"></div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
                 </div>
             </div>
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Menagoleague.pl
+# Welcome to www.Menagoleague.pl
 Menagoleague new version based on Laravel framework
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+This project is an opportunity for FIFA players to bring their gaming expieriance on new level.
+It allows gamers to compete each other base on PC, Xbox ora Playstation devices.
+Nevertheless, Menagoleague is not only a place to join tournaments. As project base function,
+Menagoleague is a simulation of becoming football Menager. You are able to take over the team,
+interact with your players and fans, you're about to achive your board goals etc.
+You will be responsible for your team brand growth as much as winning games.
+
+Visit our website for more info.
+	
+## Technologies
+Project is created with:
+* Laravel Framework
+* Bootstrap 4
+* PHP 8
+	
+## Setup
+To run this project, install it locally using npm:
+
+```
+$ cd ../lorem
+$ npm install
+$ npm start
+```
+
+
