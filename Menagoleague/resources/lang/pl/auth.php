3d2015544bea2f67749423019fb015493202c72f
Daniel <danielheinze96@gmail.com>
2021-03-19 13:18:41 +0100

    basic translations keys added

---
+++ Menagoleague/resources/lang/pl/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Wprowadzone dane są nieprawidłowe.',
+    'password' => 'Wprowadone dane są nieprawidłowe.',
+    'throttle' => 'Wprowadzono zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
+
+];
---
+++ Menagoleague/resources/lang/pl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Twoje hasło zostało zresetowane!',
+    'sent' => 'Wyslaliśmy link resetujący na Twój adres e-mail!',
+    'throttled' => 'Prosimy odczekać przed kolejnymi próbami.',
+    'token' => 'Klucz jest niepoprawny.',
+    'user' => "Podany adres e-mail nie pasuje do zadnego uzytkownika.",
+
+];
