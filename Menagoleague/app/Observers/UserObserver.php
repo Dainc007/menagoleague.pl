1bfdfca21810b99866e67205cc959199042d9e09
Daniel <danielheinze96@gmail.com>
2021-04-07 17:35:58 +0200

    observer edited

--- Menagoleague/app/Observers/UserObserver.php
+++ Menagoleague/app/Observers/UserObserver.php
@@ -11,8 +11,8 @@ class UserObserver
     public function created(User $user)
     {   
         Message::insert([
-            'title' => __('messages.boardMessages.accountCreated.title'),
-            'content' => __('messages.boardMessages.accountCreated.content'),
+            'title' => __('messages.boardMessages.accountCreated.title', ['name' => $user->name]),
+            'content' => __('messages.boardMessages.accountCreated.content', ['name' => $user->name]),
             'from' => 1,
             'to' => $user->id
         ]);
--- Menagoleague/resources/lang/pl/messages.php
+++ Menagoleague/resources/lang/pl/messages.php
@@ -4,7 +4,7 @@ return [
     'boardMessages' => [
         'accountCreated' => [
             'title'     => 'Witamy w Menago League, :name',
-            'content'   => 'Witaj :name',
+            'content'   => 'Witaj :name Przejdź kurs trenerski(samouczek), by uzyskać licencję i przejać klub',
         ]
     ]
 ];
