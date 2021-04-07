29fa5de2c3361281933f0a9510cd7afd87c92f15
Daniel <danielheinze96@gmail.com>
2021-04-07 23:18:29 +0200

    admin middleware added

---
+++ Menagoleague/app/Http/Middleware/IsAdministrator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+{
+     if (Auth::user() &&  Auth::user()->administrator == 1) {
+            return $next($request);
+     }
+
+    return redirect('/');
+}
+}
--- Menagoleague/routes/web.php
+++ Menagoleague/routes/web.php
@@ -72,12 +72,12 @@ Route::prefix('/players')->group(function () {
 Route::prefix('/articles')->group(function () {
 
     Route::get('/create', [App\Http\Controllers\ArticleController::class, 'create'])
-        ->name('article.create');
+        ->name('article.create')->middleware('administrator');
 
     Route::get('/{article}', [App\Http\Controllers\ArticleController::class, 'show'])
         ->name('article.show');
 
     Route::PUT('/{article}', [App\Http\Controllers\ArticleController::class, 'edit'])
-        ->name('article.edit');
+        ->name('article.edit')->middleware('administrator');;
 });
 
