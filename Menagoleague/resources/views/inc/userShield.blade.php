b6e37df54bbac0d4fb2e0886f18f11a891f361b7
Daniel <danielheinze96@gmail.com>
2021-04-08 15:54:13 +0200

    img added

--- Menagoleague/resources/views/inc/userShield.blade.php
+++ Menagoleague/resources/views/inc/userShield.blade.php
@@ -1,14 +1,14 @@
 @if(auth()->user()->team)
     <user-shield 
         :team="'{{auth()->user()->team->name}}'"
-        :image="'{{ asset('storage/shields/5.png') }}'"
+        :image='"{{ asset("storage/shields/ml.png") }} "'
         :username="'{{ auth()->user()->name }}'"
         :overall="100"
     />
 @else
     <user-shield 
         :team=" '{{__('home.noTeam')}}' "
-        :image="'{{ asset('storage/shields/5.png') }}'"
+        :image="'{{ asset('storage/shields/ml.png') }}'"
         :username="'{{ auth()->user()->name }}'"
         :overall="100"
     />
