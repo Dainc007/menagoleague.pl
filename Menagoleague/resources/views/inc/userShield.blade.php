844881e2437c8d9566e41f937cbe04b729c7a497
Daniel <danielheinze96@gmail.com>
2021-04-08 13:44:34 +0200

    translation keys added

--- Menagoleague/resources/views/inc/userShield.blade.php
+++ Menagoleague/resources/views/inc/userShield.blade.php
@@ -7,7 +7,7 @@
     />
 @else
     <user-shield 
-        :team="'Bez klubu'"
+        :team=" '{{__('home.noTeam')}}' "
         :image="'{{ asset('storage/shields/5.png') }}'"
         :username="'{{ auth()->user()->name }}'"
         :overall="100"
