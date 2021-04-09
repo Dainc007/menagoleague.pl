e18a5cd5b5b545912bf8c79c53254855882c2684
Daniel <danielheinze96@gmail.com>
2021-04-09 18:35:41 +0200

    articles added to welcome view

--- Menagoleague/resources/views/welcome.blade.php
+++ Menagoleague/resources/views/welcome.blade.php
@@ -8,4 +8,11 @@
 
 @include('inc.slideshow')
 
+@foreach($articles as $article)
+<h6 class="text-center">{{$article->title}}</h6>
+<p class="text-center">{{$article->content}}</p>
+<small class="text-center">{{$article->device}}</small>
+<p class="text-center">{{$article->created_at}}</p>
+@endforeach
+
 @endsection
--- Menagoleague/routes/web.php
+++ Menagoleague/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'articles' => Article::all()
+    ]);
 })->name('welcome');
 
 Auth::routes();
