a72bd282c216e26974dd3bbab81a4cab38ab867d
Daniel <danielheinze96@gmail.com>
2021-04-10 10:14:26 +0200

    translation key added

--- Menagoleague/resources/views/inc/mainNav.blade.php
+++ Menagoleague/resources/views/inc/mainNav.blade.php
@@ -94,12 +94,12 @@
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
                         <a class="dropdown-item" href="{{ route('user.account') }}">
-                            {{ __('Ustawienia Konta') }}
+                            {{ __('nav.mainNav.settings') }}
                         </a>
 
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                            {{ __('Wyloguj') }}
+                            {{ __('nav.mainNav.logout') }}
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
