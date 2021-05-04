aaa5b7734251e9252cf973aeb1aa249503aa5329
Daniel <danielheinze96@gmail.com>
2021-05-04 14:48:40 +0200

    Team View added

--- Menagoleague/resources/views/team/show.blade.php
+++ Menagoleague/resources/views/team/show.blade.php
@@ -1,2 +1,14 @@
+Druzyna
 {{$team->name}}
-{{$team->device}}
+{{$team->device_id}}
+
+Manager
+{{$team->user->name}}
+
+Rozgrywki
+{{$team->league->region}}{{$team->league->level}}
+
+Skład
+@foreach($team->players as $player)
+{{$player->name}}
+@endforeach
