014dd5c41c8dc652ae26aad83252ddc46b3e6f95
Fronikuniu <Fronikuniu@gmail.com>
2021-08-06 22:52:39 +0200

    The initial version of the squad component is completed

--- resources/views/team/myPlayers.blade.php
+++ resources/views/team/myPlayers.blade.php
@@ -1,21 +1,4 @@
-@if(!empty($user->team->players))
+<div class="squad-content">
+    @include('team.inc.teamManagement')
 
-@foreach($user->team->players as $player)
-<p> <a href="{{route('players.show', [ 'player' => $player ])}}">
-        {{$player->name}}
-        {{$player->position}}
-        {{$player->overall}}
-    </a>
-    <button class="btn-danger">Rozwiąz kontrakt</button>
-    <button class="btn-success">Przedłuz umowę</button>
-    <button class="btn-primary">Rozmawiaj</button>
-    <button class="btn-warning">Wystaw na listę transferową</button>
-    <button class="btn-secondary">Statystyki</button>
-
-</p>
-@endforeach
-
-@else
-<p>W Twojej druzynie nie ma obecnie zadnego piłkarza</p>
-
-@endif
+</div>
