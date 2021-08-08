254f674a35d4e596c55607e4daac60c5eb0a6290
Fronikuniu <Fronikuniu@gmail.com>
2021-08-08 19:17:43 +0200

    Added route for calendar

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 class OfficeController extends Controller
 {
 
+    public function calendar()
+    {
+        return view('office.calendar');
+    }
+
     public function __construct()
     {
         $this->middleware('auth');
