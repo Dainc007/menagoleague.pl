9b2cea71b4df08212bfcd9bc702d610cfeb67adf
Daniel <danielheinze96@gmail.com>
2021-08-10 20:32:19 +0200

    Translation Keys added

---
+++ resources/lang/pl/player.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'positions' => [
+        'gk'    => 'BR',
+        'lb'    => 'LO',
+        'cb'    => 'SO',
+        'rb'    => 'PO',
+        'lwd'   => 'CLS',
+        'rwd'   => 'CPS',
+        'cm'    => 'ŚP',
+        'cdm'   => 'ŚPD',
+        'cam'   => 'ŚPO',
+        'lm'    => 'LP',
+        'rm'    => 'PP',
+        'lw'    => 'LS',
+        'rw'    => 'PS',
+        'st'    => 'N',
+    ],
+
+    'actions'   => [
+        'talk'      => 'Rozmawiaj',
+        'extend'    => 'Przedłuż Kontrakt',
+        'terminate' => 'Zwolnij',
+        'sell'      => 'Wystaw na Listę Transferową',
+        'stats'     => 'Statystyki',
+    ]
+];
--- resources/views/team/team.blade.php
+++ resources/views/team/team.blade.php
@@ -4,17 +4,19 @@
 
 @section('content')
 
-    @if ($user->team)
-        @include('team.myPlayers')
-    @endif
+@if ($user->team)
+<div class="squad-content">
+    @include('team.inc.teamManagement')
 
-    @if ($user->isManager())
-        <h6>Obecnie nie jesteś zatrudniony. Przejąć klub lub złozyc wniosek o zalozenie nowego mozesz <a href="">TUTAJ</a>
-        </h6>
-    @else
-        <h6>Nie jesteś licencjonowanym trenerem. O tym jak zdobyć licencję i przejąć druzyne przeczytasz <a
-                href="">TUTAJ</a></h6>
-    @endif
+</div>
+@endif
+
+@if ($user->isManager())
+<h6>Obecnie nie jesteś zatrudniony. Przejąć klub lub złozyc wniosek o zalozenie nowego mozesz <a href="">TUTAJ</a>
+</h6>
+@else
+<h6>Nie jesteś licencjonowanym trenerem. O tym jak zdobyć licencję i przejąć druzyne przeczytasz <a href="">TUTAJ</a></h6>
+@endif
 
 
 
