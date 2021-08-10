ff28744dbd083a451a64a8164767e826162e1ac9
brambixx <kubabrambor97@o2.pl>
2021-08-10 12:03:13 +0200

    Add new items for office's page

--- resources/views/office/messages.blade.php
+++ resources/views/office/messages.blade.php
@@ -2,5 +2,7 @@
     <a href="#">
         <h2 class="office-title">Wiadomości</h2>
         <span class="material-icons">email</span>
+        <p>Masz (x) nieprzeczytanych wiadomości.</p>
+        <p>Kliknij tutaj, aby je obejrzeć!</p>
     </a>
 </section>
