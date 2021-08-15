d4be962937f8e719bc1d7b3958633e10b15e919b
Daniel <danielheinze96@gmail.com>
2021-08-15 16:45:04 +0200

    text to gp added

--- resources/views/home/inc/gameplayOverview.blade.php
+++ resources/views/home/inc/gameplayOverview.blade.php
@@ -1,5 +1,5 @@
 <div class="gameplay-overview">
-<h2>{{__('home.gameplay.title')}}</h2>
+  <h2>{{__('home.gameplay.title')}}</h2>
 
   <div class="gameplay-overview-container">
     <div class="gameplay-overview-track">
@@ -8,67 +8,34 @@
         <img src="{{ asset('storage/gameplay/central.png')}}" alt="central" />
         <h3>Intuicyjny Design</h3>
         <p>
-        Menu uzytkownika zostało stworzone i zainspirowne menu w karierze w Fifie.
+          Menu uzytkownika zostało stworzone i zainspirowne menu w karierze w Fifie.
         </p>
       </div>
       <!-- 1 -->
 
       <!-- 1 -->
       <div class="gameplay-overview-item">
-      <img src="{{ asset('storage/gameplay/team.png')}}" alt="team" />
+        <img src="{{ asset('storage/gameplay/team.png')}}" alt="team" />
         <h3>Zarzadzaj Zespołem</h3>
         <p>
-        Strona została wyposazona w przejrzysty interfejs zarzadzania zespołem, oferujący masę moliwości. Bądź dla swoich piłkarzy niczym ojciec i pomóz
-        im w rozwoju kariery lub przedkładaj losy klubu lub własne interesy.
-         Rozmawiaj z piłkarzami, negocjuj warunki kontraktów,
-        wybierz kapitana, kupuj, sprzedawaj i wiele, wiele więcej.
+          Strona została wyposazona w przejrzysty interfejs zarzadzania zespołem, oferujący masę moliwości. Bądź dla swoich piłkarzy niczym ojciec i pomóz
+          im w rozwoju kariery lub przedkładaj losy klubu lub własne interesy.
+          Rozmawiaj z piłkarzami, negocjuj warunki kontraktów,
+          wybierz kapitana, kupuj, sprzedawaj i wiele, wiele więcej.
         </p>
       </div>
       <!-- 1 -->
 
       <!-- 1 -->
       <div class="gameplay-overview-item">
-      <img src="{{ asset('storage/gameplay/season.png')}}" alt="season" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
+        <img src="{{ asset('storage/gameplay/season.png')}}" alt="season" />
+        <h3>Przeglądaj Pozostałe Ligi</h3>
         <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
+          Korzystaj z łatwego przeglądu statystyk zarówno własnych rozgrywek jak i swoich znajomych grających w innych ligach.
         </p>
       </div>
       <!-- 1 -->
 
-      <!-- 1 -->
-      <div class="gameplay-overview-item">
-        <img src="{{url('/images/6.png')}}" alt="" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
-        </p>
-      </div>
-      <!-- 1 -->
-
-      <!-- 1 -->
-      <div class="gameplay-overview-item">
-        <img src="{{url('/images/7.png')}}" alt="" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
-        </p>
-      </div>
-      <!-- 1 -->
-
-      <!-- 6 -->
-      <div class="gameplay-overview-item">
-        <img src="{{url('/images/8.png')}}" alt="" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
-        </p>
-      </div>
-      <!-- 6 -->
     </div>
   </div>
 </div>
