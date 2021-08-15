a831eaabb4a6a198cfc7eee949becba947f12b8f
Daniel <danielheinze96@gmail.com>
2021-08-15 16:06:59 +0200

    new images

--- resources/views/home/inc/gameplayOverview.blade.php
+++ resources/views/home/inc/gameplayOverview.blade.php
@@ -5,29 +5,30 @@
     <div class="gameplay-overview-track">
       <!-- 1 -->
       <div class="gameplay-overview-item">
-        <img src="{{url('/images/3.png')}}" alt="" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
+        <img src="{{ asset('storage/gameplay/central.png')}}" alt="central" />
+        <h3>Intuicyjny Design</h3>
         <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
+        Menu uzytkownika zostało stworzone i zainspirowne menu w karierze w Fifie.
         </p>
       </div>
       <!-- 1 -->
 
       <!-- 1 -->
       <div class="gameplay-overview-item">
-        <img src="{{url('/images/4.png')}}" alt="" />
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
+      <img src="{{ asset('storage/gameplay/team.png')}}" alt="team" />
+        <h3>Zarzadzaj Zespołem</h3>
         <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
-        praesentium alias perspiciatis esse dolorum facere voluptates libero quae? Corrupti, praesentium eaque eligendi quisquam nobis ea.
+        Strona została wyposazona w przejrzysty interfejs zarzadzania zespołem, oferujący masę moliwości. Bądź dla swoich piłkarzy niczym ojciec i pomóz
+        im w rozwoju kariery lub przedkładaj losy klubu lub własne interesy.
+         Rozmawiaj z piłkarzami, negocjuj warunki kontraktów,
+        wybierz kapitana, kupuj, sprzedawaj i wiele, wiele więcej.
         </p>
       </div>
       <!-- 1 -->
 
       <!-- 1 -->
       <div class="gameplay-overview-item">
-        <img src="{{url('/images/5.png')}}" alt="" />
+      <img src="{{ asset('storage/gameplay/season.png')}}" alt="season" />
         <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, repellendus!</h3>
         <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ex itaque laboriosam sed quo laudantium omnis unde architecto repudiandae, cupiditate numquam ullam expedita hic
