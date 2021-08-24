fa7eee42d43805e7da21ed2b694bfcd044c73904
brambixx <kubabrambor97@o2.pl>
2021-08-24 22:07:32 +0200

    Add player chat - in progress

---
+++ resources/views/office/messages/list.blade.php
@@ -0,0 +1,76 @@
+<section class="messages-list">
+    <div class="players-list">
+        <div class="message active">
+            <div class="message-logo">
+                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
+            </div>
+            <div class="message-details">
+                <div class="title">
+                    <span>David Beckham</span>
+                </div>
+                <div class="info">
+                    <span>
+                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
+                    </span>
+                </div>
+            </div>
+            <div class="message-date">
+                <span>16.08</span>
+            </div>
+        </div>
+        <div class="message">
+            <div class="message-logo">
+                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
+            </div>
+            <div class="message-details">
+                <div class="title">
+                    <span>David Beckham</span>
+                </div>
+                <div class="info">
+                    <span>
+                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
+                    </span>
+                </div>
+            </div>
+            <div class="message-date">
+                <p>16.08</p>
+            </div>
+        </div>
+        <div class="message">
+            <div class="message-logo">
+                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
+            </div>
+            <div class="message-details">
+                <div class="title">
+                    <span>David Beckham</span>
+                </div>
+                <div class="info">
+                    <span>
+                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
+                    </span>
+                </div>
+            </div>
+            <div class="message-date">
+                <p>16.08</p>
+            </div>
+        </div>
+        <div class="message">
+            <div class="message-logo">
+                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
+            </div>
+            <div class="message-details">
+                <div class="title">
+                    <span>David Beckham</span>
+                </div>
+                <div class="info">
+                    <span>
+                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
+                    </span>
+                </div>
+            </div>
+            <div class="message-date">
+                <p>16.08</p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/office/messages/text.blade.php
@@ -0,0 +1,6 @@
+<section class="messages-text">
+    <span>tab1</span>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ipsam, ut sequi ea at iusto minus amet culpa, pariatur in deserunt ratione consequatur hic cumque tempore, tenetur porro aperiam asperiores!
+    </p>
+</section>
