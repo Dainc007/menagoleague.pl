1d861d6dd3946850500374d055dbbcc6c2d71487
brambixx <kubabrambor97@o2.pl>
2021-08-25 13:09:29 +0200

    Add player chat

--- resources/views/office/messages/list.blade.php
+++ resources/views/office/messages/list.blade.php
@@ -18,59 +18,40 @@
                 <span>16.08</span>
             </div>
         </div>
-        <div class="message">
-            <div class="message-logo">
-                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
-            </div>
-            <div class="message-details">
-                <div class="title">
-                    <span>David Beckham</span>
+        @for ($i=0;$i<=10;$i++)
+            <div class="message">
+                <div class="message-logo">
+                    <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
                 </div>
-                <div class="info">
-                    <span>
-                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
-                    </span>
+                <div class="message-details">
+                    <div class="title">
+                        <span>David Beckham</span>
+                    </div>
+                    <div class="info">
+                        <span>
+                            {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <div class="message-date">
-                <p>16.08</p>
-            </div>
-        </div>
-        <div class="message">
-            <div class="message-logo">
-                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
-            </div>
-            <div class="message-details">
-                <div class="title">
-                    <span>David Beckham</span>
-                </div>
-                <div class="info">
-                    <span>
-                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
-                    </span>
+                <div class="message-date">
+                    <p>16.08</p>
                 </div>
             </div>
-            <div class="message-date">
-                <p>16.08</p>
-            </div>
-        </div>
-        <div class="message">
-            <div class="message-logo">
-                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
-            </div>
-            <div class="message-details">
-                <div class="title">
-                    <span>David Beckham</span>
-                </div>
-                <div class="info">
-                    <span>
-                        {{Str::limit('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repellat ', 35, '...')}}
-                    </span>
-                </div>
-            </div>
-            <div class="message-date">
-                <p>16.08</p>
-            </div>
+        @endfor
+        <div class="messages-controls">
+            <a href="#" class="disabled">
+                <span class="material-icons">
+                    skip_previous
+                </span>
+                Poprzednia strona 
+                
+            </a>
+            <a href="#">
+                Następna strona
+                <span class="material-icons">
+                    skip_next
+                </span>
+            </a>
         </div>
     </div>
 </section>
--- resources/views/office/messages/text.blade.php
+++ resources/views/office/messages/text.blade.php
@@ -1,6 +1,58 @@
 <section class="messages-text">
-    <span>tab1</span>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ipsam, ut sequi ea at iusto minus amet culpa, pariatur in deserunt ratione consequatur hic cumque tempore, tenetur porro aperiam asperiores!
-    </p>
+    <div class="text-entry">
+        <div class="player-info">
+            <div class="player-overall">
+                <p>OGL</p>
+                <p class="overall">
+                    82
+                </p>
+            </div>
+            <div class="player-photo">
+                <img src="{{ url('/images/shields/' . rand(1, 8) . '.png') }}">
+            </div>
+            <div class="player-about">
+                <p class="name">
+                    Silvan
+                </p>
+                <p class="surname">
+                    HEFTI
+                </p>
+                <p class="position">
+                    <span class="circle orange">
+    
+                    </span>
+                    <span class="shortcut">
+                        PO
+                    </span>
+                    <span class="pos-type">
+                        Rotacyjny
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="player-morals">
+            <p>Morale</p>
+            <span class="progress-bar">
+
+            </span>
+            <p>
+                Szczęśliwy
+            </p>
+        </div>
+    </div>
+    <div class="text-full">
+        <div class="receiver">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae est mollitia, magnam, at accusantium nisi officiis unde adipisci qui corrupti excepturi aliquam ratione! Molestias quia nisi quidem alias fugiat dignissimos!
+            </p>
+        </div>
+        <div class="sender">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A minima harum illum perspiciatis quod similique, ipsum assumenda voluptatum, veritatis exercitationem optio quo reprehenderit magnam itaque possimus accusamus ex dolor laudantium!
+            </p>
+        </div>
+        <div class="reply">
+
+        </div>
+    </div>
 </section>
