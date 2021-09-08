9f245a7abccdee6776218f88d69f28b6e3ef5c45
Daniel <danielheinze96@gmail.com>
2021-09-08 14:04:56 +0200

    button name property added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
 
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button name="login" type="submit" class="btn btn-primary">
                             {{ __('auth.signIn') }}
                             </button>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -135,7 +135,7 @@
 
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button name="register" type="submit" class="btn btn-primary">
                                 {{ __('auth.join') }}
                             </button>
                         </div>
