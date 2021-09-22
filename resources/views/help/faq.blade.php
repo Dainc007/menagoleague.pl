7d780aec1d288dee8cba13c6e619b62154c68846
Fronikuniu <Fronikuniu@gmail.com>
2021-09-22 23:36:22 +0200

    Change structure of ranking component, move some code to other file

--- resources/views/help/faq.blade.php
+++ resources/views/help/faq.blade.php
@@ -5,30 +5,6 @@
     <div class="ranking-content">
         <h1>Ranking</h1>
 
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="rank-tab" data-toggle="tab" href="#rank" role="tab" aria-controls="rank"
-                    aria-selected="true">Gracze</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="rank-teams-tab" data-toggle="tab" href="#rank-teams" role="tab"
-                    aria-controls="rank-teams" aria-selected="false">Drużyny</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="rank-players-tab" data-toggle="tab" href="#rank-players" role="tab"
-                    aria-controls="rank-players" aria-selected="false">Piłkarze</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="rank" role="tabpanel" aria-labelledby="rank-tab">
-                @include('.help.inc.ranking.rank')
-            </div>
-            <div class="tab-pane fade" id="rank-teams" role="tabpanel" aria-labelledby="rank-teams-tab">
-                @include('.help.inc.ranking.rankTeams')
-            </div>
-            <div class="tab-pane fade" id="rank-players" role="tabpanel" aria-labelledby="rank-players-tab">
-                @include('.help.inc.ranking.rankPlayers')
-            </div>
-        </div>
+        @include('.help.inc.ranking.rankTabs')
     </div>
 @endsection
