0da6fe72823faf4aa479b66bc10c8a6de4f986b0
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-06 18:11:22 +0100

    tutorial

--- resources/views/help/tutorial.blade.php
+++ resources/views/help/tutorial.blade.php
@@ -1,33 +1,71 @@
-<h6> Jak zdobyć licencję trenerską? </h6>
+@extends('layouts.homeLayout')
 
-<p>Aby umożliwić Ci udział w naszych rozgrywkach, musimy mieć pewność, że rozumiesz zasady panujące w Menago League.</p>
-<p>tym celu każdy gracz rozpoczyna swoją przygodę od rozegrania meczu towarzyskiego z innym użytkownikiem.</p>
-<p>Jeśli masz już sparing partnera możesz zaprosić go do meczu używając formularza zaznaczonego w poniższej ilustracji
-    na czerwono, wybierając przeciwnika z listy i klikając "zaproś".</p>
-<p> Jeśli jeszcze nie masz sparing partnera, z pewnością znajdziesz go na naszym kanale Discord lub grupie na Facebooku
-    (linki zaznaczone na zielono i niebiesko)</p>
+@section('content')
+    <div class="container">
+        <div class="row mb-3 shadow p-3 rounded">
+            <h1 class="col text-center"> Jak zdobyć licencję trenerską? </h1>
+        </div>
+        <div class="row mb-3 shadow  p-3 rounded">
+            <div class="col">
+                <p class="text-justify font-weight-bold"> Aby umożliwić Ci udział w naszych rozgrywkach, musimy mieć pewność,
+                    że rozumiesz
+                    zasady panujące w Menago League.<br>
+                    tym celu każdy gracz rozpoczyna swoją przygodę od rozegrania meczu towarzyskiego z innym
+                    użytkownikiem.<br>
+                    Jeśli masz już sparing partnera możesz zaprosić go do meczu używając formularza zaznaczonego w poniższej
+                    ilustracji
+                    na czerwono, wybierając przeciwnika z listy i klikając "zaproś".
+                    Jeśli jeszcze nie masz sparing partnera, z pewnością znajdziesz go na naszym kanale Discord lub grupie
+                    na
+                    Facebooku
+                    (linki zaznaczone na zielono i niebiesko).</p>
+            </div>
+            <div class="col">
+                <img class="img img-fluid" src="{{ asset('storage/tutorial/tutorial1.png') }}">
+            </div>
+        </div>
 
-<img class="img img-fluid" src="{{ asset('storage/tutorial/tutorial1.png') }}">
+        <div class="row mb-3 shadow rounded p-3">
+            <div class="col-4">
+                <img class="img img-fluid" src="{{ asset('storage/tutorial/tutorial2.png') }}">
+            </div>
+            <div class="col-8">
+                <p class="text-justify font-weight-bold">Teraz jesteś gotowy, by rozegrać mecz z przeciwnikiem. Skontaktuj
+                    się z nim i
+                    dodaj go do swojej listy znajomych na
+                    platformie, którą wykorzystujesz do gry (Xbox lub Playstation)
+                    Następnie jeden z was powinien wysłać zaproszenie do meczu w grze FIFA. Wazne, by wysyłać zaproszenie
+                    zachowując
+                    ponizsze ustawienia:</p>
+                <img class="img img-fluid mt-3" src="{{ asset('storage/tutorial/settings.png') }}">
 
-<p>Teraz jesteś gotowy, by rozegrać mecz z przeciwnikiem. Skontaktuj się z nim i dodaj go do swojej listy znajomych na
-    platformie, którą wykorzystujesz do gry (Xbox lub Playstation)</p>
+                <p class="text-justify font-weight-bold my-3">
+                    Jeśli w połączeniu z przeciwnikiem wystąpią jakieś trudności, upewnijcie się, że wasz tryb translacji
+                    nat jest
+                    „otwarty” lub „umiarkowany”.Więcej na temat rozwiązywania problemów z NAT przeczytasz <a>TUTAJ</a>.
+                </p>
 
-<p> Następnie jeden z was powinien wysłać zaproszenie do meczu w grze FIFA. Wazne, by wysyłać zaproszenie zachowując
-    ponizsze ustawienia:</p>
-<img class="img img-fluid" src="{{ asset('storage/tutorial/settings.png') }}">
-<p>
-    Jeśli w połączeniu z przeciwnikiem wystąpią jakieś trudności, upewnijcie się, że wasz tryb translacji nat jest „otwarty” lub „umiarkowany”.Więcej na temat rozwiązywania problemów z NAT przeczytasz <a>TUTAJ</a>.
-</p>
-<h6>UWAGA</h6>
-<p>Zanim rozpoczniecie mecz zapoznaj się z poniższymi klipami.Są to przykładowe klipy, które powinieneś nagrać w trakcie meczu z przeciwnikiem.</p>
+                <div class="alert alert-danger" role="alert">
+                    UWAGA! Zanim rozpoczniecie mecz zapoznaj się z poniższymi klipami.Są to przykładowe klipy, które
+                    powinieneś
+                    nagrać w trakcie
+                    meczu z przeciwnikiem.
+                </div>
+                <p class="text-justify font-weight-bold">
+                    1. pomeczowy raport statystyk (warto nagrać równiez w przerwie)<br>
+                    2. dowód uczciwej gry.<br>
+                    Kiedy juz rozegrałeś mecz i nagrałeś odpowiednie klipy, mozesz złozyc aplikacje o przyznanie licencji
+                    trenerskiej
+                    wypełniając ponizszy formularz.
 
-1. pomeczowy raport statystyk (warto nagrać równiez w przerwie)
-2. dowód uczciwej gry.
+                    Kiedy już wyślesz wniosek pozostaje Ci jedynie czekać na jego weryfikacje.Kiedy takowa nastąpi otrzymasz
+                    powiadomienie na ten temat.W międzyczasie zapoznaj się z poradnikami:- Jak przejąć drużynę?- Jak
+                    przenieść mój skład
+                    Menago League do gry FIFA?
+                </p>
+            </div>
+        </div>
+    </div>
 
-<p>Kiedy juz rozegrałeś mecz i nagrałeś odpowiednie klipy, mozesz złozyc aplikacje o przyznanie licencji trenerskiej wypełniając ponizszy formularz.</p>
-<img class="img img-fluid" src="{{ asset('storage/tutorial/tutorial2.png') }}">
-
-<p> Kiedy już wyślesz wniosek pozostaje Ci jedynie czekać na jego weryfikacje.Kiedy takowa nastąpi otrzymasz powiadomienie na ten temat.W międzyczasie zapoznaj się z poradnikami:- Jak przejąć drużynę?- Jak przenieść mój skład Menago League do gry FIFA?</p>
-
-
-<p> budowanie druzyny w grze fifa <a href="https://www.youtube.com/watch?v=skcLsskPkA8&t=11s">Tutaj</a></p>
+    <p> budowanie druzyny w grze fifa <a href="https://www.youtube.com/watch?v=skcLsskPkA8&t=11s">Tutaj</a></p>
+@endsection
