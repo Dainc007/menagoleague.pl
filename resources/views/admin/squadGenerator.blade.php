b7e631510a68bc52242ecdf224de7e7eed57d506
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-08 08:55:07 +0100

    view changes

--- resources/views/admin/squadGenerator.blade.php
+++ resources/views/admin/squadGenerator.blade.php
@@ -2,35 +2,36 @@
 
 @section('content')
     <div class="container">
-        @foreach ($squads as $squad)
-            @php
-                $avg = $squad->avg('overall');
-                $pace = $squad->avg('paceTotal');
-            @endphp
-
-            <div class="row mb-3">
-                <div class="col text-center">
-                    <h6> sredni ovr:{{ $avg }}</h6>
-                    <h6> srednie tempo:{{ $pace }}</h6>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-
-                @foreach ($squad as $player)
-                    <div class="col-3 text-center">
-                        <h3><img class="img img-fluid" style="width:50px;"
-                                src="https://fifastatic.fifaindex.com/FIFA22/players/{{ $player->id }}.png"></h3>
-                        <h4>{{ $player->name }}({{ $player->bestPosition }})</h4>
-                        <h5>ovr: {{ $player->overall }}</h5>
-                        <h5>tempo: {{ $player->paceTotal }}</h5>
+        <div class="row">
+            @foreach ($squads as $squad)
+                @php
+                    $avg = $squad->avg('overall');
+                    $pace = $squad->avg('paceTotal');
+                @endphp
+                <div class="col-4 shadow bg-white">
+                    <div class="row mb-3">
+                        <div class="col text-center">
+                            <h6> sredni ovr:{{ Str::limit($avg, 4, '') }}</h6>
+                            <h6> srednie tempo:{{ Str::limit($pace, 4, '') }}</h6>
+                        </div>
                     </div>
-                @endforeach
 
-            </div>
+                    <div class="row mb-3">
+
+                        @foreach ($squad as $player)
+                            <div class="col-6 text-center">
+                                <h4><img class="img img-fluid" style="width:30px;"
+                                        src="https://fifastatic.fifaindex.com/FIFA22/players/{{ $player->id }}.png"></h4>
+                                <h5>{{ $player->name }}</h5>
+                                <h6>({{ $player->bestPosition }})</h6>
+                                <h6>ovr: {{ $player->overall }}</h6>
+                                <h6>tempo: {{ $player->paceTotal }}</h6>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
-
-
-    @endforeach
 @endsection
