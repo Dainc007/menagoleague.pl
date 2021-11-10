7645aeee6da623b192cc99fbb0cdde713b40fca3
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-10 22:51:31 +0100

    last commit today

--- resources/views/team/team.blade.php
+++ resources/views/team/team.blade.php
@@ -4,23 +4,27 @@
 
 @section('content')
 
-@if ($user->team)
-<div class="squad-content">
-    @include('team.inc.teamManagement')
+    @if ($user->team)
+        <div class="squad-content">
+            @include('team.inc.teamManagement')
 
-</div>
-@endif
+        </div>
+    @endif
 
-@if ($user->isManager())
-<div class="alert alert-info" role="alert">
-    <h6> {{__('team.noJob')}} <a href="{{route('help.tutorial') }}">
-    </h6>
-  </div>
+    @if (!$user->isManager())
+        <div class="alert alert-info" role="alert">
+            <h6>{{ __('team.noLicence') }}
+                <a href="{{ route('help.tutorial') }}">{{ __('team.here') }}</a>
+            </h6>
+        </div>
+    @endif
 
-@else
-<h6>{{__('team.noLicence')}}
-     <a href="{{route('help.tutorial') }}">{{__('team.here')}}</a></h6>
-@endif
+    @if (!$user->team)
+        <div class="alert alert-info" role="alert">
+            <h6> {{ __('team.noJob') }} <a href="{{ route('help.tutorial') }}"></a>
+            </h6>
+        </div>
+    @endif
 
 
 
