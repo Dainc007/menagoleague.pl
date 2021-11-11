52e1ebf8cfe96b70ca5770220f92861df498f1de
Fronikuniu <Fronikuniu@gmail.com>
2021-11-11 17:24:35 +0100

    test

--- storage/framework/views/eddc69737c3357174f6bdad35ca1590b211fbc14.php
+++ storage/framework/views/eddc69737c3357174f6bdad35ca1590b211fbc14.php
@@ -1,66 +1,185 @@
 <div class="gameplay-overview">
-  <h2><?php echo e(__('home.gameplay.title')); ?></h2>
+    <h2><?php echo e(__('home.gameplay.title')); ?></h2>
 
-  <div class="gameplay-overview-container">
-    <div class="gameplay-overview-track">
-      <!-- 1 -->
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/central.png')); ?>" alt="central" />
-        <h3><?php echo e(__('home.gameplay.1.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.1.body')); ?>
+    <div class="gameplay-overview-container">
+        <div class="gameplay-overview-track">
+            <!-- 1 -->
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/central.png')); ?>" alt="central" data-toggle="modal"
+                    data-target="#gameplay-1" />
+                <h3><?php echo e(__('home.gameplay.1.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.1.body')); ?>
 
-        </p>
-      </div>
-      <!-- 1 -->
+                </p>
 
-      <!-- 1 -->
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/team.png')); ?>" alt="team" />
-        <h3><?php echo e(__('home.gameplay.2.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.2.body')); ?>
+                <div class="modal fade" id="gameplay-1" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.1.header')); ?>
 
-        </p>
-      </div>
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/central.png')); ?>" alt="central" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 1 -->
 
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/season.png')); ?>" alt="season" />
-        <h3><?php echo e(__('home.gameplay.3.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.3.body')); ?>
+            <!-- 1 -->
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/team.png')); ?>" alt="team" data-toggle="modal"
+                    data-target="#gameplay-2" />
+                <h3><?php echo e(__('home.gameplay.2.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.2.body')); ?>
 
-        </p>
-      </div>
+                </p>
 
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/office.png')); ?>" alt="office" />
-        <h3><?php echo e(__('home.gameplay.4.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.4.body')); ?>
+                <div class="modal fade" id="gameplay-2" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.2.header')); ?>
 
-        </p>
-      </div>
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/team.png')); ?>" alt="team" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/calendar.png')); ?>" alt="office" />
-        <h3><?php echo e(__('home.gameplay.5.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.5.body')); ?>
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/season.png')); ?>" alt="season" data-toggle="modal"
+                    data-target="#gameplay-3" />
+                <h3><?php echo e(__('home.gameplay.3.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.3.body')); ?>
 
-        </p>
-      </div>
+                </p>
 
-      <div class="gameplay-overview-item">
-        <img src="<?php echo e(asset('storage/gameplay/playerView.png')); ?>" alt="PlayerView" />
-        <h3><?php echo e(__('home.gameplay.6.header')); ?></h3>
-        <p>
-        <?php echo e(__('home.gameplay.6.body')); ?>
+                <div class="modal fade" id="gameplay-3" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.3.header')); ?>
 
-        </p>
-      </div>
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/season.png')); ?>" alt="season" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/office.png')); ?>" alt="office" data-toggle="modal"
+                    data-target="#gameplay-4" />
+                <h3><?php echo e(__('home.gameplay.4.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.4.body')); ?>
 
+                </p>
+
+                <div class="modal fade" id="gameplay-4" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.4.header')); ?>
+
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/office.png')); ?>" alt="office" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/calendar.png')); ?>" alt="callendar" data-toggle="modal"
+                    data-target="#gameplay-5" />
+                <h3><?php echo e(__('home.gameplay.5.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.5.body')); ?>
+
+                </p>
+
+                <div class="modal fade" id="gameplay-5" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.5.header')); ?>
+
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/calendar.png')); ?>" alt="calendar" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="gameplay-overview-item">
+                <img src="<?php echo e(asset('storage/gameplay/playerView.png')); ?>" alt="playerView" data-toggle="modal"
+                    data-target="#gameplay-6" />
+                <h3><?php echo e(__('home.gameplay.6.header')); ?></h3>
+                <p>
+                    <?php echo e(__('home.gameplay.6.body')); ?>
+
+                </p>
+
+                <div class="modal fade" id="gameplay-6" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel"><?php echo e(__('home.gameplay.6.header')); ?>
+
+                                </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo e(asset('storage/gameplay/playerView.png')); ?>" alt="player view" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div><?php /**PATH C:\Users\Bartek\Desktop\KURS\Menagoleague.pl\resources\views/home/inc/gameplayOverview.blade.php ENDPATH**/ ?>
+</div>
+<?php /**PATH C:\Users\Bartek\Desktop\KURS\Menagoleague.pl\resources\views/home/inc/gameplayOverview.blade.php ENDPATH**/ ?>
