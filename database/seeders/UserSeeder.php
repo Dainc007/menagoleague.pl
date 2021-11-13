9fa699e30028bc7ee2e8df5631423664c492274a
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-13 19:16:06 +0100

    tutorial

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(100)->create();
+     /*   \App\Models\User::factory(100)->create();*/
         \App\Models\Role::factory(5)->create();
     }
 }
--- resources/views/help/tutorial.blade.php
+++ resources/views/help/tutorial.blade.php
@@ -47,7 +47,7 @@
                 <p>
                     Jeśli w połączeniu z przeciwnikiem wystąpią jakieś trudności, upewnijcie się, że wasz tryb translacji
                     nat jest „otwarty” lub „umiarkowany”. Więcej na temat rozwiązywania problemów z NAT przeczytasz
-                    <a href="#">TUTAJ</a>.
+                    <a target="_blank" href="https://support.xbox.com/pl-PL/help/hardware-network/connect-network/xbox-one-nat-error">TUTAJ</a>.
                 </p>
 
                 <div class="alert alert-danger" role="alert">
@@ -75,6 +75,140 @@
                 </p>
             </div>
         </div>
+    </div>
+
+
+    <div class="container">
+        <div class="tutorial-header">
+            <h1 class="col text-center"> Rozgrywanie Meczu z przeciwnikiem </h1>
+        </div>
+
+        <div class="tutorial one">
+            <div class="tutorial__text">
+                <div class="alert alert-success" role="alert">
+              Gospodarz spotkania wysyła przeciwnikowi zaproszenie do meczu. Jeśli wystąpią problemy z połączeniem,
+                    zaproszenie może wysłać również gość.
+                </div>
+                <div class="alert alert-info" role="alert">
+                    Aby mieć możliwość zaproszenia przeciwnika do meczu najpierw dodaj go do <b>listy znajomych</b> na swojej konsoli.
+                </div>
+
+                <div class="alert alert-danger" role="alert">
+                    Zapraszając przeciwnika do meczu pamiętaj o właściwych ustawieniach:
+                    <b>Składy: Własne</b> <br>
+                    <b>Długość Połowy: 6 minut</b> <br>
+                    <b>Tempo gry: Normalne</b>
+                </div>
+
+            </div>
+            <div class="tutorial__img">
+                <img class="img img-fluid" src="https://thumbs.gfycat.com/UniformSkinnyGhostshrimp-size_restricted.gif">
+            </div>
+        </div>
+
+        <div class="tutorial two">
+            <div class="tutorial__img">
+                <img src="https://thumbs.gfycat.com/EnragedAccomplishedAtlanticridleyturtle-size_restricted.gif">
+            </div>
+            <div class="tutorial__text">
+                <div class="alert alert-success" role="alert">
+                    Po rozegraniu pierwszej połowy przejdź do odpowiedniej sekcji i nagraj statystyki spotkania.
+                    Interesuje nas podsumowanie obu drużyn, oraz statystyki piłkarzy obu drużyn, którzy pojawili się już na boisko.
+                </div>
+                <div class="alert alert-danger" role="alert">
+                    <b>UWAGA!</b> Statystyki po 1 połowie wrzucamy wraz z wynikiem meczu tylko i wyłacznie, jeśli wystąpił problem
+                    z nagraniem statystyk po zakończeniu spotkania. Jest to dla nas dodatkowe zabezpieczenie, gdyż każdy brak statystyk
+                    pomeczowych może być traktowany jako obustronny walkower, z którego Twoi piłkarze będą bardzo niezadowoleni!
+                </div>
+            </div>
+        </div>
+
+
+        <div class="tutorial two">
+            <div class="tutorial__text">
+                <div class="alert alert-info" role="alert">
+                    Mecz uznaje się za zakończony, gdy po ostatnim gwizdku pominięte zostaną powtórki oraz obaj gracze
+                    zaznaczą odpowiedź "NIE" w pytaniu, które pojawi się w menu: "Czy chcesz zagrać rewanż?" lub po upływie
+                    15 sekund od pojawienia się pytania.
+                </div>
+                <div class="alert alert-danger" role="alert">
+                    <b>UWAGA!</b> Wyjście z meczu przed zaznaczeniem odpowiedzi lub upływem czasu zostanie odebrane przez grę
+                    jako wyjście z meczu co spowoduje niezapisanie wyniku. Taka praktyka jest surowo zabroniona w naszych rozgrywkach
+                    i może skutkować walkowerem lub w ostateczności wykluczeniem z rozgrywek.
+                </div>
+            </div>
+
+            <div class="tutorial__img">
+                <img src="https://thumbs.gfycat.com/IdenticalIllegalKingbird-size_restricted.gif">
+            </div>
+        </div>
+
+
+        <div class="tutorial two">
+            <div class="tutorial__img">
+                <img src="https://thumbs.gfycat.com/YawningIncompleteGharial-size_restricted.gif">
+            </div>
+            <div class="tutorial__text">
+
+                <div class="alert alert-success" role="alert">
+                    Nagranie Statystyk po zakończeniu spotkania jest bliźniaczo podobne do statystyk po pierwszej połowie.
+                    Nagrywamy "Statystyki meczu" oraz indywidualne statystyki wszystkich piłkarzy, którzy wzieli udział w grze
+                </div>
+                <div class="alert alert-info" role="alert">
+                    Każdy gracz, który nagrywa statystyki zarówno swoje jak i przeciwnika ma u nas wielkiego <b>+</b> !
+                </div>
+            </div>
+        </div>
+
+        <div class="tutorial two">
+            <div class="tutorial__text">
+                <div class="alert alert-info" role="alert">
+                    Czasem może się zdarzyć (dotychczas 2 przypadki na kilkaset meczów) że przeciwnik poprosi Cię o dowód,
+                    że grasz zgodnie z przepisami. W tym celu Twój przeciwnik prześle Ci wiadomość na Twojej konsoli
+                    (lub innym komunikatorze, jeśli tak ustalicie przed spotkaniem) z prośbą o nagranie poniższych klipów.
+                    Powinieneś na takim klipie zamieścić szczegółowe dane istotnych umiejętności wszystkich piłkarzy Twojej
+                    drużyny, którzy wzięli udział w meczu.
+                </div>
+                <div class="alert alert-danger" role="alert">
+                    Poprzez "istotne umiejętności" rozumiemy umiejętności bramkarskie w przypadku bramkarza i tym podobne.
+                </div>
+
+                <div class="alert alert-danger" role="alert">
+                    <b>Uwaga!</b> Osoby przyłapane na oszukiwaniu lub niedostarczające notorycznie dowodów uczciwej gry zostają
+                    permamentnie usunięte z rozgrywek.
+                </div>
+            </div>
+
+            <div class="tutorial__img">
+                <img class="mb-3" src="https://thumbs.gfycat.com/MatureSinfulBrownbear-size_restricted.gif">
+                <img src="https://thumbs.gfycat.com/UniformFlatAndalusianhorse-size_restricted.gif">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="tutorial-header">
+            <h1 class="col text-center"> Przenoszenie składu Menago League do gry FIFA </h1>
+        </div>
+
+        <div class="tutorial one">
+            <div class="tutorial__text">
+                <div class="alert alert-success" role="alert">
+                    W tej kwestii nie zmieniło się absolutnie nic. W związku z czym odsyłamy do naszego krótkiego
+                    poradnika na Youtube jeszcze z gry FIFA18
+                </div>
+
+
+            </div>
+            <div class="tutorial__img">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/skcLsskPkA8"
+                        title="YouTube video player"
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+            </div>
+        </div>
+
     </div>
 
     {{-- <p> budowanie druzyny w grze fifa <a href="https://www.youtube.com/watch?v=skcLsskPkA8&t=11s">Tutaj</a></p> --}}
