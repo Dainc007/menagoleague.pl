e4e3a4f46be8131e6f44ce06d543b64ecadd503c
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-16 12:07:22 +0100

    admin view fixed

--- resources/views/admin/tutorials.blade.php
+++ resources/views/admin/tutorials.blade.php
@@ -7,9 +7,11 @@
 
 <form method="POST" action="{{ route('admin.tutorial.respond', ['id' => $tutorial->id]) }}">
     <div class="form-group">
-        {{$tutorial->fullTime}}
-        {{$tutorial->halfTime}}
-        {{$tutorial->fairPlay}}
+        <p><a href="{{$tutorial->full_time}}" target="_blank">Staty po meczu</a> </p>
+
+        <p><a href="{{$tutorial->half_time}}" target="_blank">Połowa</a> </p>
+
+        <p><a href="{{$tutorial->fair_play}}" target="_blank">Uczciwa Gra</a> </p>
         <input type="text" name="response" required>
         <input type="submit" class="btn btn-sm btn-success" name="accept" value="akceptuj">
         <input type="submit" class="btn btn-sm btn-danger" name="reject" value="odrzuć">
