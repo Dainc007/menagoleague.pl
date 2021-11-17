40885a444295ceb1196eb8d2743bb3d727069f40
Daniel Heinze <daniel.he@modulesgarden.com>
2021-11-17 10:39:01 +0100

    cookies

--- resources/views/help/privacy.blade.php
+++ resources/views/help/privacy.blade.php
@@ -1,1 +1,26 @@
-privacy policy
+<small>Polityka prywatności serwisu www. menagoleague.pl</small>
+<h4>Informacje ogólne</h4>
+<p>1. Niniejsza Polityka Prywatności określa zasady przetwarzania i ochrony danych osobowych przekazanych przez Użytkowników w związku z korzystaniem przez nich usług menagoleague.pl poprzez Serwis.
+</p>
+<p>    2. Administratorem danych osobowych zawartych w serwisie jestGrupa Menago League.
+</p>
+<p>3. W trosce o bezpieczeństwo powierzonych nam danych opracowaliśmy wewnętrzne procedury i zalecenia, które mają zapobiec udostępnieniu danych osobom nieupoważnionym. Kontrolujemy ich wykonywanie i stale sprawdzamy ich zgodność z odpowiednimi aktami prawnymi - ustawą o ochronie danych osobowych, ustawą o świadczeniu usług drogą elektroniczną, a także wszelkiego rodzaju aktach wykonawczych i aktach prawa wspólnotowego.
+</p>
+<p>4. Dane Osobowe przetwarzane są na podstawie zgody wyrażanej przez Użytkownika oraz w przypadkach, w których przepisy prawa upoważniają Administratora do przetwarzania danych osobowych na podstawie przepisów prawa lub w celu realizacji zawartej pomiędzy stronami umowy.
+</p>
+<p>5. Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniach w następujący sposób:
+    poprzez dobrowolnie wprowadzone w formularzach informacje
+    poprzez gromadzenie plików “cookies” [patrz polityka plików “cookies”].</p>
+
+<p>6. Serwis zbiera informacje dobrowolnie podane przez użytkownika.
+</p>
+<p>7. dane podane w formularzu są przetwarzane w celu wynikającym z funkcji konkretnego formularza np. w celu dokonania procesu obsługi kontaktu informacyjnego
+</p>
+<p>8. Dane osobowe pozostawione w serwisie nie zostaną sprzedane ani udostępnione osobom trzecim, zgodnie z przepisami Ustawy o ochronie danych osobowych.
+</p>
+<p>9. Do danych zawartych w formularzu przysługuje wgląd osobie fizycznej, która je tam umieściła. Osoba ta ma również praw do modyfikacji i zaprzestania przetwarzania swoich danych w dowolnym momencie.
+</p>
+<p>10. Zastrzegamy sobie prawo do zmiany w polityce ochrony prywatności serwisu, na które może wpłynąć rozwój technologii internetowej, ewentualne zmiany prawa w zakresie ochrony danych osobowych oraz rozwój naszego serwisu internetowego. O wszelkich zmianach będziemy informować w sposób widoczny i zrozumiały.
+</p>
+<p>11. W Serwisie mogą pojawiać się linki do innych stron internetowych. Takie strony internetowe działają niezależnie od Serwisu i nie są w żaden sposób nadzorowane przez serwis Menago League Strony te mogą posiadać własne polityki dotyczące prywatności oraz regulaminy, z którymi zalecamy się zapoznać.
+    W razie wątpliwości co któregokolwiek z zapisów niniejszej polityki prywatności jesteśmy do dyspozycji - nasze dane znaleźć można w zakładce - KONTAKT.</p>
