ed4a26789425641aa16baa85c9beadfa34d7527c
Fronikuniu <Fronikuniu@gmail.com>
2021-11-18 13:20:10 +0100

    Fix styles and add some details

--- resources/views/home/inc/news.blade.php
+++ resources/views/home/inc/news.blade.php
@@ -1,77 +1,85 @@
 <div class="content">
-  <section class="league-news">
-    <div class="league-news-h">
-      <h2>{{__('home.news.header')}}</h2>
+    <section class="league-news">
+        <div class="league-news-h">
+            <h2>{{ __('home.news.header') }}</h2>
 
-      <div class="league-news-devices">
-        <a class="device" href=""><img src="./images/xbox-icon.svg" alt="Xbox" /></a>
-        <a class="device" href=""><img src="./images/playstation-icon.svg" alt="Playstation" /></a>
-        <a class="device" href=""><img src="./images/pc-icon.svg" alt="PC" /></a>
-      </div>
-    </div>
-
-    <div class="league-news-container">
-      @if($articles !== null)
-      @foreach ($articles as $article)
-      <div class="league-news-item" data-toggle="modal" data-target="#exampleModalLong{{$article->id}}">
-        <img class="league-news-item-img"
-             @if($article->id == 5)
-             src="{{url('/images/managers.webp')}}"
-             @else
-             src="{{url('/images/'. rand(1,8) .'.webp')}}"
-             @endif
-             alt="" />
-
-        <div class="league-news-item-shadow">
-          <p class="league-news-item-p">{{$article->title}}</p>
-        </div>
-        
-
-        <div class="modal fade" id="exampleModalLong{{$article->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h4 class="modal-title" id="myLargeModalLabel">{{$article->title}}</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">×</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <img
-                        @if($article->id == 5)
-                        src="{{url('/images/managers.png')}}"
-                        @else
-                        src="{{url('/images/'. rand(1,8) .'.webp')}}"
-                        @endif alt="" />
-                <p> {{$article->content}}</p>
-                <h5 class="pt-3 pb-1 mb-1"><span> <small> Source: Sky Sports Menago </small></span>
-                  <small>{{$article->created_at->format('d-m-Y')}}</small></h5>
-              </div>
+            <div class="league-news-devices">
+                <a class="device" href=""><img src="./images/xbox-icon.svg" alt="Xbox" /></a>
+                <a class="device" href=""><img src="./images/playstation-icon.svg" alt="Playstation" /></a>
+                <a class="device" href=""><img src="./images/pc-icon.svg" alt="PC" /></a>
             </div>
-          </div>
         </div>
-      </div>
-      @endforeach
-      @endif
-    </div>
-    <a href="" class="btn btn-w-border btn-allNews responsive-btn-allNews">{{__('home.news.moreNews')}}</a>
-  </section>
 
-  <aside class="read-more">
-    <h2>{{__('home.news.readMore')}}</h2>
+        <div class="league-news-container">
+            @if ($articles !== null)
+                @foreach ($articles as $article)
+                    <div class="league-news-item" data-toggle="modal"
+                        data-target="#exampleModalLong{{ $article->id }}">
+                        <img class="league-news-item-img" @if ($article->id == 5)
+                        src="{{ url('/images/managers.webp') }}"
+                    @else src="{{ url('/images/' . rand(1, 8) . '.webp') }}"
+                @endif
+                alt="" />
+
+                {{-- Tutaj wyswietlanie ikonek dla urządzeń --}}
+                <div class="device-img"><img src="./images/xbox-icon.svg" alt="Xbox" /></div>
+
+                <div class="league-news-item-shadow">
+                    <p class="league-news-item-p">{{ $article->title }}</p>
+                </div>
+
+
+                <div class="modal fade" id="exampleModalLong{{ $article->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="myLargeModalLabel">{{ $article->title }}</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img @if ($article->id == 5)
+                                src="{{ url('/images/managers.png') }}"
+                            @else
+                                src="{{ url('/images/' . rand(1, 8) . '.webp') }}"
+                                @endif alt="" />
+                                <p> {{ $article->content }}</p>
+                                <h5 class="pt-3 pb-1 mb-1"><span> <small> Source: Sky Sports Menago </small></span>
+                                    <small>{{ $article->created_at->format('d-m-Y') }}</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        @endforeach
+        @endif
+</div>
+<a href="" class="btn btn-w-border btn-allNews responsive-btn-allNews">{{ __('home.news.moreNews') }}</a>
+</section>
+
+<aside class="read-more">
+    <h2>{{ __('home.news.readMore') }}</h2>
 
     <div class="read-more-container">
-      @foreach ($oldArticles as $article) <div class="read-more-item">
-        <img class="read-more-item-img" src="{{url('/images/'. rand(1,8) .'.webp')}}" alt="" />
-        <div class="read-more-item-text">
-          <p>{{$article->created_at->format('d-m-Y')}}</p>
+        @foreach ($oldArticles as $article)
+            <div class="read-more-item">
+                <img class="read-more-item-img" src="{{ url('/images/' . rand(1, 8) . '.webp') }}" alt="" />
+                <div class="read-more-item-text">
+                    <p>
+                        {{ $article->created_at->format('d-m-Y') }}
+                        {{-- Tutaj wyswietlanie ikonek dla urządzeń --}}
+                        <img src="./images/xbox-icon.svg" alt="Xbox" />
+                    </p>
 
-          <a class="read-more-item-text-a" href="">{{$article->title}}.</a>
-        </div>
-      </div>
-      @endforeach
+                    <a class="read-more-item-text-a" href="">{{ $article->title }}.</a>
+                </div>
+            </div>
+        @endforeach
 
     </div>
-    <a href="{{ route('article.index') }}" class="btn btn-w-border btn-allNews">{{__('home.news.moreNews')}}</a>
-  </aside>
+    <a href="{{ route('article.index') }}" class="btn btn-w-border btn-allNews">{{ __('home.news.moreNews') }}</a>
+</aside>
 </div>
--- resources/views/team/inc/attributes.blade.php
+++ resources/views/team/inc/attributes.blade.php
@@ -2,42 +2,41 @@
     <div class="player-attributes-container">
 
         <div class="player-attributes-desc">
-            <p class="pos">{{__('team.table.position')}}</p>
+            <p class="pos">{{ __('team.table.position') }}</p>
 
-            <p class="name">{{__('team.table.name')}}</p>
+            <p class="name">{{ __('team.table.name') }}</p>
 
-            <p class="overall">{{__('team.table.age')}}</p>
+            <p class="age">{{ __('team.table.age') }}</p>
 
-            <p class="overall">{{__('team.table.ovr')}}</p>
+            <p class="overall">{{ __('team.table.ovr') }}</p>
 
-            <p class="overall">{{__('team.table.nationality')}}</p>
-
-
-
+            <p class="nationality">{{ __('team.table.nationality') }}</p>
         </div>
 
         <div class="player-attributes-content">
             @if ($user->team->players !== null)
-            @foreach ($user->team->players as $player)
+                @foreach ($user->team->players as $player)
 
-            <div class="player-attributes-item">
-                <p class="pos">{{ __('player.positions.' . $player->playerDetails->bestPosition) }}</p>
+                    <div class="player-attributes-item">
+                        <p class="pos">{{ __('player.positions.' . $player->playerDetails->bestPosition) }}
+                        </p>
 
-                <a class="name" href="{{ route('players.show', ['player' => $player]) }}">
-                    <img class="player-image" src="{{env('PLAYER_SRC') . $player->playerDetails->id}}.png" alt="">
-                    {{ $player->name }}
-                </a>
+                        <a class="name" href="{{ route('players.show', ['player' => $player]) }}">
+                            <img class="player-image" src="{{ env('PLAYER_SRC') . $player->playerDetails->id }}.png"
+                                alt="">
+                            {{ $player->name }}
+                        </a>
 
-                <p class="age">{{ $player->playerDetails->age }}</p>
+                        <p class="age">{{ $player->playerDetails->age }}</p>
 
-                <p class="overall">{{ $player->playerDetails->overall }}</p>
+                        <p class="overall">{{ $player->playerDetails->overall }}</p>
 
-                <p class="nationality">{{ $player->playerDetails->nationality }}</p>
-            </div>
+                        <p class="nationality">{{ $player->playerDetails->nationality }}</p>
+                    </div>
 
-            @endforeach
+                @endforeach
             @else
-            <p>{{__('team.empty')}}W Twojej druzynie nie ma obecnie zadnego piłkarza</p>
+                <p>{{ __('team.empty') }}W Twojej druzynie nie ma obecnie zadnego piłkarza</p>
             @endif
         </div>
     </div>
